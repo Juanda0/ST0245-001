03b4fac90f1f1537c209d34dde20fdfd75f2850e
Juanda0 <61121948+Juanda0@users.noreply.github.com>
2020-08-06 20:30:15 -0500

    Create Punto3.py

---
+++ talleres/taller02/Punto3.py
@@ -0,0 +1,21 @@
+i = 0
+j = 0
+def subconjuntosBase(base, palabra):
+    if len(palabra) == 0:       
+        print (base)
+    else:
+        subconjuntosBase(base + palabra[i],palabra[i+1:len(palabra)])
+        subconjuntosBase(base, palabra[j+1:len(palabra)])
+        
+       
+def subconjuntos(s):
+    subconjuntosBase("", s)
+
+ 
+
+
+print(subconjuntos("Alex"))
+print(subconjuntos("Santi"))
+print(subconjuntos("abc"))
+
+#profe, no logramos eliminar el "None" de la impresion :c. Supongamos que no exite c:
