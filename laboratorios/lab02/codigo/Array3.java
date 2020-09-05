41e7f0fe7cd5fb064e188f2f6596e5d4816a8bda
Mioso22 <68928402+Mioso22@users.noreply.github.com>
2020-09-04 23:22:14 -0500

    Update Array3.java

--- laboratorios/lab02/codigo/Array3.java
+++ laboratorios/lab02/codigo/Array3.java
@@ -29,3 +29,54 @@ public boolean canBalance(int[] nums){
   return false;
 }
 
+public int maxSpan(int[] nums) {
+  int m = 0;
+    for(int i = 0; i < nums.length; i++) {
+      int j = nums.length - 1;
+      while(nums[i] != nums[j])
+        j--;
+        int s = j - i + 1;
+        if(s > m)
+          m = s;
+  }
+  return m;
+}
+
+public int[] fix45(int[] nums) {
+  int i = 0;
+  int j = 0;
+  while(j < nums.length && nums[j] != 5)
+    j++;
+    while(i < nums.length) {
+      if(nums[i] == 4) {
+        int t = nums[i+1];
+        nums[i+1] = nums[j];
+        nums[j] = t;
+        while((j < nums.length && nums[j] != 5) || j == i + 1)
+          j++;
+      }
+      i++;
+  }
+  return nums;
+}
+
+public int[] fix34(int[] nums) {
+  int i = 0;
+  while(i < nums.length && nums[i] != 3)
+    i++;
+  int j = i + 1;
+  while(j < nums.length && nums[j] != 4)
+    j++;
+  while(i < nums.length) {
+    if(nums[i] == 3) {
+      int temp = nums[i+1];
+      nums[i+1] = nums[j];
+      nums[j] = temp;
+      while(j < nums.length && nums[j] != 4)
+        j++;
+      }
+      i++;
+  }
+  return nums;
+}
+
